40608ed97138350a111fd2a8cd6ef45af2c4a178
MarahNajjar21 <78256592+MarahNajjar21@users.noreply.github.com>
2022-07-24 15:27:27 +0300

    Update front.py

--- front.py
+++ front.py
@@ -5,6 +5,7 @@ app = Flask(_name_)
 @app.route("/")
 def home():
     return "hello world"
+
 @app.route("/info/<id>",methods=['GET'])
 def info(id):
     idInt=int(id)
@@ -22,16 +23,6 @@ def purchase():
      idInt=int(id)
      result=requests.get("http://192.168.1.240:7001/purchase/{}".format(idInt))
      return (result.content)
-
-@app.route("/queryNumbers",methods=['PUT'])
-def queryNumbers():
-     id=request.json['ID']
-     idInt=int(id)
-     amount=request.json['AMOUNTS']
-     amountInt=int(amount)
-     result=requests.put("http://192.168.1.240:7001/queryNumbers",data={'ID':idInt,'AMOUNTS':amountInt})
-     return (result.content)
-
 
 if _name_ == 'main':
     app.run(debug=True, host='0.0.0.0', port=5000)
