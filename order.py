834bc91036167b44413f7f5f10ee5c8233c78db3
MarahNajjar21 <78256592+MarahNajjar21@users.noreply.github.com>
2022-07-30 21:03:07 +0300

    Update order.py

--- order.py
+++ order.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from xml.dom.minidom import TypeInfo
 from flask import Flask, request, jsonify
 import json
@@ -13,11 +12,14 @@ def home():
 @app.route("/purchase/<id>",methods=['GET'])
 def purchase(id):
     idInt = int(id)
-    result=requests.get("http://192.168.1.250:7002/info/{}".format(idInt))
-    if(result.json()["info"][0]["NUMBERS"]>0):
-        buyResult=requests.put("http://192.168.1.250:7002/queryNumbers/{}".format(idInt),data={'AMOUNTS':1})
-        return buyResult.content
-    else :return ({"msg":"out of stock!!"})
+    #result=requests.get("http://192.168.1.240:7010/info/{}".format(idInt))
+    #if(result.json()["info"][0]["NUMBERS"]>0):
+    buyResult=requests.put("http://192.168.137.68:7018/queryNumbers/{}".format(idInt),data=json.dumps({'AMOUNTS':1}))
+    type(json.dumps({'AMOUNTS':1}))
+    return buyResult.content
+    #text,buyResult.status_code,buyResult.headers.items()
+    
+    #else :return ({"msg":"out of stock!!"})
     
 if _name_ == '_main_':
 
