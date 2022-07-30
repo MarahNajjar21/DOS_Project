d5305ef87b1b9d16175bf9d33475e7af19dd44e3
MarahNajjar21 <78256592+MarahNajjar21@users.noreply.github.com>
2022-07-30 21:02:00 +0300

    Update front.py

--- front.py
+++ front.py
@@ -5,24 +5,25 @@ app = Flask(_name_)
 @app.route("/")
 def home():
     return "hello world"
-
 @app.route("/info/<id>",methods=['GET'])
 def info(id):
     idInt=int(id)
-    result=requests.get("http://192.168.1.250:7002/info/{}".format(idInt))
+    result=requests.get("http://192.168.137.68:7018/info/{}".format(idInt))
     return (result.content)
-
 @app.route("/search/<topic>",methods=['GET'])
 def search(topic):
-    result=requests.get("http://192.168.1.250:7002/search/{}".format(topic))
+    result=requests.get("http://192.168.137.68:7018/search/{}".format(topic))
     return (result.content)
 
-@app.route("/purchase/<id>",methods=['GET'])
-def purchase():
+@app.route("/purchase/<int:id>",methods=['GET'])
+def purchase(id):
      id=request.json['ID']
      idInt=int(id)
-     result=requests.get("http://192.168.1.240:7001/purchase/{}".format(idInt))
+     result=requests.get("http://192.168.137.73:7001/purchase/{}".format(idInt))
      return (result.content)
+
+
+
 
 if _name_ == 'main':
     app.run(debug=True, host='0.0.0.0', port=5000)
